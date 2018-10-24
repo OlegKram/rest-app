cdd300fd42b8a394441fcadd11bd50dddf3aa5d8
OlegKram <kramoleh@gmail.com>
2018-10-24 19:54:51 +0300

    Configuration authorization

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -4,6 +4,7 @@ function getIndex(list, id) {
             return i;
         }
     }
+
     return -1;
 }
 
@@ -89,13 +90,6 @@ Vue.component('messages-list', {
     '<message-row v-for="message in messages" :key="message.id" :message="message" ' +
     ':editMethod="editMethod" :messages="messages" />' +
     '</div>',
-    created: function() {
-        messageApi.get().then(result =>
-        result.json().then(data =>
-        data.forEach(message => this.messages.push(message))
-    )
-    )
-    },
     methods: {
         editMethod: function(message) {
             this.message = message;
@@ -105,8 +99,19 @@ Vue.component('messages-list', {
 
 var app = new Vue({
     el: '#app',
-    template: '<messages-list :messages="messages" />',
+    template:
+    '<div>' +
+    '<div v-if="!profile">Required authorization <a href="/login">Login with Google</a></div>' +
+    '<div v-else>' +
+    '<div>{{profile.name}}&nbsp;<a href="/logout">Logout</a></div>' +
+    '<messages-list :messages="messages" />' +
+    '</div>' +
+    '</div>',
     data: {
-        messages: []
-    }
+        messages: frontendData.messages,
+        profile: frontendData.profile
+    },
+    created: function() {
+
+    },
 });
